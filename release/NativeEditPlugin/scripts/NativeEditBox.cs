da3130fb289462a05773f1ae990ce3f8f3578c6b
Oskari Leppäaho <oskari@yousician.com>
2016-11-07 13:23:32 +0200

    Default return key as default case in switch case

--- release/NativeEditPlugin/scripts/NativeEditBox.cs
+++ release/NativeEditPlugin/scripts/NativeEditBox.cs
@@ -258,10 +258,6 @@ public class NativeEditBox : PluginMsgReceiver {
 
 		switch (iosReturnKeyType)
 		{
-			case ReturnKeyType.Default:
-				jsonMsg["return_key_type"] = "Default";
-				break;
-
 			case ReturnKeyType.Next:
 				jsonMsg["return_key_type"] = "Next";
 				break;
@@ -271,6 +267,7 @@ public class NativeEditBox : PluginMsgReceiver {
 				break;
 
 			default:
+				jsonMsg["return_key_type"] = "Default";
 				break;
 		}
 
